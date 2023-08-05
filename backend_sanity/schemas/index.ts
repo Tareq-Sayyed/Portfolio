7f5495938af59aaacfabfe90b703c94648a3f183
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2023-08-05 09:24:16 +0300

    Created the backend using sanity studio and created the schemas. Secondly, I created the react app for the frontend and removed the unnecessary files.

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,13 @@
+import { createSchema } from 'sanity'
+import testimonials from './testimonials'
+import about from  './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [testimonials, about, brands, contact, experiences, skills, workExperience, works]
+
+
